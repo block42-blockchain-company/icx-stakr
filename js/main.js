fbb2ec964ba77e28a50f025f6b35cbc945c53ad9
Christian Lanz <christian.l4nz@gmail.com>
2019-08-28 17:17:49 +0200

    Add calculation logic

--- js/main.js
+++ js/main.js
@@ -13,7 +13,24 @@ function fetchIconNetworkData() {
 
       console.log("Staked Supply: " + stakedSupply + " ICX");
 
-      // Do some fun
+      const staked = stakedSupply / totalSupply * 100;
+
+      $("#staked").val(numeral(staked).format("0.00"));
     })
   })
+}
+
+function calculate() {
+  const r_min = 0.02, r_max = 0.12, r_point = 0.7;
+
+  let staked = $("#staked").val();
+  if(staked >= 70) staked = 70;
+
+  const r_rep = ((r_max-r_min) / (r_point ** 2)) * ((staked / 100) - r_point) ** 2 + r_min;
+  const roi = (r_rep * 100 * 3).toFixed(2); // Assuming same delegation rates for other two sectors, hence * 3
+
+  const holdings = $("#holdings").val();
+  const rewards = holdings * (roi / 100);
+
+  console.log(rewards + " ICX");
 }
